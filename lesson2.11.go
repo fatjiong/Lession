9bf24f111fb321f61273ac18323408c751ab784f
fatjiong <757970599@qq.com>
2018-01-24 18:05:09 +0800

    [学习] 开始go并发编程实战第二版

---
+++ lesson2.11.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+//聊天机器人
+
+
+
+func main(){
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please input your name:")
+	//读取数据到\n为止
+	input ,err :=inputReader.ReadString('\n')
+	if err != nil {
+		fmt.Printf("An error occurred: %s\n",err)
+		os.Exit(1)
+	}else{
+		// 用切片删除最后的\n
+		name := input[:len(input)-1]
+		fmt.Printf("Hello,%s! What can I do for you?\n",name)
+	}
+
+	for {
+	input,err :=inputReader.ReadString('\n')
+	if err != nil{
+		fmt.Printf("An error occurred: %s\n",err)
+		continue
+	}
+
+	input = input[:len(input)-1]
+	// 全部转换为小写
+	input = strings.ToLower(input)
+	switch input{
+	case "":
+		continue
+	case "nothing","bye":
+		fmt.Printf("Bye!");
+		os.Exit(0)
+	default:
+		fmt.Println("Sorry, I didn't catch you.")
+	}
+	}
+
+
+
+	}
